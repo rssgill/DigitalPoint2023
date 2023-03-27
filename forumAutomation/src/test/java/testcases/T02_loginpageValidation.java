542d6e84a014c4479b0c4184b5c5f68a2135a7b7
GRFC <GRFC@10.0.0.88>
2023-03-26 21:49:38 -0400

    T02

--- forumAutomation/src/test/java/testcases/T02_loginpageValidation.java
+++ forumAutomation/src/test/java/testcases/T02_loginpageValidation.java
@@ -3,6 +3,7 @@ package testcases;
 import org.testng.annotations.Test;
 
 import Utilities.browserEngine;
+import Utilities.forumURL;
 
 import org.testng.AssertJUnit;
 import org.openqa.selenium.By;
@@ -31,24 +32,75 @@ public static WebDriver driver;
 	public static void loginpage()
 	{
 		
-		driver.get("https://forum.digitalpoint.tech/user/index.php/login");
+		//load the application under test 
 		
-		String loginpageATitle = "Digital Point - Forum | User Login";
-		String loginpageBTitle = driver.getTitle();
-		
-		AssertJUnit.assertEquals(loginpageATitle, loginpageBTitle);
-		
-		//identify the email textbox and send the data into this textbox 
-		
-				driver.findElement(By.name("email")).sendKeys("nizam.mahmood@gmail.com");
+				System.out.println("Browser is opening Home page.");
+				
+				driver.get(forumURL.homepageURL);
+				
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				System.out.println("Browser is opening Login page.");
+				
+				driver.findElement(By.xpath("//*[@id=\'text-fade\']/div[1]/div/div[3]/div/a[2]")).click();
+				
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				//driver.get(forumURL.loginpageURL);
+				
+				//home page title validation 
+				
+				String loginpageETitle ="Digital Point - Forum | User Login";
+				
+				String loginpageATitle=driver.getTitle();
+				
+				AssertJUnit.assertEquals(loginpageATitle, loginpageETitle);
+				
+				
+				System.out.println("Driver is logging in.");
+				
+				//identify the email textbox and send the data into this testbox 
+							
+				driver.findElement(By.name("email")).sendKeys(forumURL.loginUid);
 				
 				//identify the password textbox and send the value into this password textbox 
 				
-				driver.findElement(By.name("password")).sendKeys("abcdef*123456");
+				driver.findElement(By.name("password")).sendKeys(forumURL.loginPsswd);
 				
 				//identify the signin button and click the button 
 				
 				driver.findElement(By.xpath("/html/body/div[2]/div/div/form/div/div[2]/div[3]/div[2]/button")).click();
+				
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				System.out.println("Driver is logging out.");
+				
+				driver.findElement(By.xpath("//*[@id=\"navbar-right\"]/li/a/span")).click();
+				
+				driver.findElement(By.xpath("//*[@id=\"navbar-right\"]/li/ul/li/a")).click();
+				
+				
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		
 	}
 	
